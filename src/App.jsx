6befdeb35ed69913c0edd16b14630afa919f599d
masum-hosen <masumhosen2333@gmail.com>
2025-02-02 21:37:31 +0600

    This is initialize commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Banner from './Banner';
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="mb-[80vh]">
+        <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      </div>
+      <Banner />
+    </>
+  );
+}
+
+export default App
---
+++ src/Banner.jsx
@@ -0,0 +1,8 @@
+function Banner(){
+    return (
+      <div className="bg-teal-950 text-3xl color text-green-300 text-center p-8 ">
+        <p>This is Banner</p>
+      </div>
+    );
+}
+export default Banner;
